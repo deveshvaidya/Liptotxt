8d9f2eefe15166c0f3e54289ba94ffba49f84c88
atb824 <74148169+atb824@users.noreply.github.com>
2021-07-02 01:07:51 +0530

    ,model removed

--- imageNetProj/urls.py
+++ imageNetProj/urls.py
@@ -23,8 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('^$',views.index,name='homepage'),
-    url('predictImage',views.predictImage,name='predictImage'),
-    url('viewDataBase',views.viewDataBase,name='viewDataBase'),
+    # url('predictImage',views.predictImage,name='predictImage'),
+    # url('viewDataBase',views.viewDataBase,name='viewDataBase'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
